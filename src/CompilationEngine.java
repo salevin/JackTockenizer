8fe5be4913318af61fcfc0d9c47d626000d0d794
johnSturgis <jsturg402@verizon.net>
2016-02-08 11:38:07 -0500

    Comments are now ignored and strings are tokens

--- src/CompilationEngine.java
+++ src/CompilationEngine.java
@@ -8,5 +8,11 @@ public class CompilationEngine {
 
     public void compileClass(){
         //TODO
+        /*
+        The compileClass will contain a single while loop which loops while the tokenizer
+        has more tokens and writes the results from the tokenizer to the output file.
+        Since the argument to your code can be either a .jack file or a directory containing
+        multiple .jack files, the following main program will deal with those two cases.
+         */
     }
 }
