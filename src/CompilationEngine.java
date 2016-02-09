e28a068eb2e707a3ef024ee8e6fea83880075d89
johnSturgis <jsturg402@verizon.net>
2016-02-09 14:59:59 -0500

    Start of project 2

--- src/CompilationEngine.java
+++ src/CompilationEngine.java
@@ -1,3 +1,5 @@
+import com.sun.xml.internal.bind.v2.TODO;
+
 import java.io.BufferedWriter;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -43,4 +45,70 @@ public class CompilationEngine {
             System.err.println(x);
         }
     }
+
+    public void compileClassVarDec(){
+        //TODO
+        //Compiles a static declaration or a field declaration.
+    }
+
+    public void compileSubroutine(){
+        //TODO
+        //Compiles a complete method,function, or constructor.
+    }
+
+    public void compileParameterList(){
+        //TODO
+        //Compiles a (possibly empty) parameter list, not including the enclosing ‘‘ () ’’.
+    }
+
+    public void compileVarDec(){
+        // TODO Compiles a var declaration.
+    }
+
+    public void compileStatements(){
+        // TODO Compiles a sequence of statements, not including the enclosing ‘‘{}’’.
+    }
+
+    public void compileDo(){
+        // TODO compiles a do statement
+    }
+
+    public void compileLet(){
+        // TODO compiles a let statement
+    }
+
+    public void compileWhile(){
+        // TODO
+    }
+
+    public void compileReturn(){
+        // TODO
+    }
+
+    public void compileIf(){
+        // TODO
+    }
+
+    public void compileExpression(){
+        // TODO
+    }
+
+    public void compileTerm(){
+        // TODO
+        /*Compiles a term. This routine is faced with a slight difficulty
+        when trying to decide between some of the alternative parsing
+        rules. Specifically, if the current token is an identifier, the routine
+        must distinguish between a variable, an array entry, and a
+        subroutine call. A single look- ahead token, which may be one
+        of ‘‘[’’, ‘‘(’’, or ‘‘.’’ suffices to distinguish between the three possi-
+        bilities. Any other token is not part of this term and should not
+        be advanced over.*/
+    }
+
+    public void compileExpressionList(){
+        // TODO
+        //Compiles a (possibly empty) comma-separated list of expressions.
+    }
+
+
 }
